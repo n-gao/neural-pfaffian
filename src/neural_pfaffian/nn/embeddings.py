64b31df18e61fd443f18ee94dcb8caa78d1b8712
Nicholas Gao <nicholas.gao96@gmail.com>
2025-03-19 08:41:45 +0100

    remove unused file

--- src/neural_pfaffian/nn/embeddings.py
+++
@@ -1,9 +0,0 @@
-from neural_pfaffian.nn.ferminet import FermiNet
-from neural_pfaffian.nn.moon import Moon
-from neural_pfaffian.nn.psiformer import PsiFormer
-from neural_pfaffian.nn.wave_function import EmbeddingP
-from neural_pfaffian.utils import Modules
-
-EMBEDDINGS = Modules[EmbeddingP](
-    {embedding.__name__.lower(): embedding for embedding in [FermiNet, Moon, PsiFormer]}
-)
